5d9298cb9400c1da0bc3580d0ece059605b570be
AkshayToms <89342459+AkshayToms@users.noreply.github.com>
2021-10-11 15:49:49 +0530

    Create kube.tf

---
+++ kube.tf
@@ -0,0 +1,45 @@
+Main.tf-------------------------------------- 
+
+provider "google" {,  
+
+project = "pragmatic-will-324605" 
+
+} 
+
+ 
+
+resource "google_service_account" "default" { 
+
+account_id = "pragmatic-will-324605" 
+
+display_name = "test-terraform2" 
+
+} 
+
+ 
+
+resource "google_container_cluster" "primary" { 
+
+name = "my-gke-cluster2" 
+
+location = "us-central1" 
+
+remove_default_node_pool = true 
+
+initial_node_count = 1 
+
+} 
+
+ 
+
+resource "google_container_node_pool" "primary_preemptible_nodes" { 
+
+name = "my-node-pool" 
+
+location = "us-central1" 
+
+cluster = google_container_cluster.primary.name 
+
+node_count = 1 
+
+} 
