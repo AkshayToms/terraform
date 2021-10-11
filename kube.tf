687be0eda62e5b331cda833ac2635f1ea16289f7
AkshayToms <89342459+AkshayToms@users.noreply.github.com>
2021-10-11 16:11:46 +0530

    Update kube.tf

--- kube.tf
+++ kube.tf
@@ -1,45 +1,28 @@
-Main.tf-------------------------------------- 
+provider "google" {
+    project     = "pragmatic-will-324605"
+}
 
-provider "google" {,  
+resource "google_service_account" "default1" {
+  account_id   = "pragmatic-will-324605"
+  display_name = "test-terraform2"
+}
 
-project = "pragmatic-will-324605" 
+resource "google_container_cluster" "primary1" {
+  name     = "my-gke-cluster2"
+  location = "us-central1"
 
-} 
+  
+  remove_default_node_pool = true
+  initial_node_count       = 1
+}
 
- 
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = "my-node-pool"
+  location   = "us-central1"
+  cluster    = google_container_cluster.primary.name
+  node_count = 1
 
-resource "google_service_account" "default" { 
+  
+}
 
-account_id = "pragmatic-will-324605" 
 
-display_name = "test-terraform2" 
-
-} 
-
- 
-
-resource "google_container_cluster" "primary" { 
-
-name = "my-gke-cluster2" 
-
-location = "us-central1" 
-
-remove_default_node_pool = true 
-
-initial_node_count = 1 
-
-} 
-
- 
-
-resource "google_container_node_pool" "primary_preemptible_nodes" { 
-
-name = "my-node-pool" 
-
-location = "us-central1" 
-
-cluster = google_container_cluster.primary.name 
-
-node_count = 1 
-
-} 
